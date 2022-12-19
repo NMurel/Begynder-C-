46e25591b7f61ebce44ccc84994b83dbf288cbc0
NMurel <murelnikolaj@gmail.com>
2022-12-19 18:07:32 +0100

    First commit

---
+++ Begynder CSharp/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Begynder_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] heldigenummere = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Console.WriteLine(heldigenummere[7]);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
